b52327db996b0bd211808122f35e020c118e9924
Angela Velasquez <angela.velasquez@laureate.net>
2018-10-10 08:13:04 -0600

    Removing unnecessary images and adittional updates

--- lionware/js/index.js
+++ lionware/js/index.js
@@ -1,18 +1,18 @@
 
 if(window.location.pathname === '/lionware/silverbird') {
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wp').forEach( el => el.classList.remove('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.remove('hidden') );
 };
 
 if(window.location.pathname === '/lionware/aquagem') {
-    document.querySelectorAll('.wp').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.remove('hidden') );
 };
 
-if(window.location.pathname === '/lionware/warehousingpro') {
+if(window.location.pathname === '/lionware/forklift') {
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wp').forEach( el => el.classList.remove('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
 };
